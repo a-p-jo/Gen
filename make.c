afd08e23af4eab234dc9fb8f6fcc907a1726bfb2
A.P. Jo <66941562+a-p-jo@users.noreply.github.com>
2021-02-01 09:32:11 +0530

    Update make.c

--- make.c
+++ make.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
-	printf("\n Mode : "); int mode; scanf("%d",&mode);
 
-	if(mode==1){
-		system("cc -Wall -o3 file1.c file2.c -o bin_file_name");
+int main(int frequency, char * arguments[])
+{
+	if(frequency >= 2)
+	{
+		int mode = strtol(arguments[1],NULL,10);
+		
+		if(mode==1)
+		{
+			system("cc -Wall -o3 file1.c file2.c -o binary_filename");
+		}
+		
+		else if(mode==2)
+		{
+			system("cc -Wall -Wextra -pedantic file1.c file2.c -o binary_filename");
+		}
+		
+		else if(mode==3)
+		{
+			system("cc -Wall -Wextra -pedantic -Wconversion -o0 file1.c file2.c -o binary_filename");
+		}
 	}
-	else if(mode==2){
-		system("cc -Wall -Wextra -pedantic file1.c file2.c -o bin_file_name");
+	else
+	{
+		printf("Not enough arguments.\n");
 	}
-	else if(mode==3){
-		system("cc -Wall -Wextra -pedantic -Wconversion -o0 file1.c file2.c -o bin_file_name");
-	}
+	
 	return 0;
 }
